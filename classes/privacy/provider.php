9f48e28a6c95d089f6cb6f8204b78615fd997730
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2024-01-23 20:21:28 +0200

    fix class namespace

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace report_completion\privacy;
+namespace report_completionall\privacy;
 
 /**
  * Privacy provider implementation for report_completionall.

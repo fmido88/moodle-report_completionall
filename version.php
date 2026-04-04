faf81ed7b9d0753377fc2ac5d09e6b7303b0e78d
Mohammad Farouk <phun.for.physics@gmail.com>
2026-04-05 00:24:45 +0200

    Correct version name

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2026040400;
-$plugin->release   = '1.0.3';
+$plugin->release   = '5.1.0';
 $plugin->requires  = 2025100600;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'report_completionall';
